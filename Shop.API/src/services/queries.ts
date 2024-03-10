a8a94b6eb0e9e6918a58df618fe7a1f227bdc934
HomeMac <mauserez@mail.ru>
2024-03-10 23:33:59 +0300

    first commit

---
+++ Shop.API/src/services/queries.ts
@@ -0,0 +1,79 @@
+export const COMMENT_DUPLICATE_QUERY = `
+  SELECT * FROM comments c
+  WHERE LOWER(c.email) = ?
+  AND LOWER(c.name) = ?
+  AND LOWER(c.body) = ?
+  AND c.product_id = ?
+`;
+
+export const INSERT_COMMENT_QUERY = `
+  INSERT INTO comments
+  (comment_id, email, name, body, product_id)
+  VALUES
+  (?, ?, ?, ?, ?)
+`;
+
+export const INSERT_PRODUCT_QUERY = `
+  INSERT INTO products
+  (product_id, title, description, price)
+  VALUES
+  (?, ?, ?, ?)
+`;
+
+export const INSERT_PRODUCT_IMAGES_QUERY = `
+  INSERT INTO images
+  (image_id, url, product_id, main)
+  VALUES ?
+`;
+
+export const DELETE_IMAGES_QUERY = `
+  DELETE FROM images
+  WHERE image_id IN ?;
+`;
+
+export const REPLACE_PRODUCT_THUMBNAIL = `
+  UPDATE images
+  SET main = CASE
+    WHEN image_id = ? THEN 0
+    WHEN image_id = ? THEN 1
+    ELSE main
+  END
+  WHERE image_id IN (?, ?);
+`;
+
+export const UPDATE_PRODUCT_FIELDS = `
+    UPDATE products
+    SET title = ?, description = ?, price = ?
+    WHERE product_id = ?
+`;
+
+export const GET_SIMILAR_PRODUCTS_QUERY = `
+  SELECT
+    p.product_id,
+    p.title,
+    p.description,
+    p.price
+  FROM products p
+  JOIN similar_products sp on
+  (p.product_id = sp.similar_product_id and sp.product_id = ?)
+  or
+  (p.product_id = sp.product_id and sp.similar_product_id = ?)
+`;
+
+export const INSERT_SIMILAR_PRODUCTS_QUERY = `
+  INSERT similar_products
+  (product_id,similar_product_id)
+  VALUES
+  (?,?)
+`;
+
+export const DELETE_SIMILAR_PRODUCTS_BY_IDS_QUERY = `
+  DELETE FROM similar_products
+  WHERE product_id = ?
+  and similar_product_id = ?
+`;
+
+export const DELETE_SIMILAR_PRODUCTS_QUERY = `
+  DELETE FROM similar_products
+  WHERE product_id = ? or similar_product_id = ?
+`;
---
+++ Shop.API/types.ts
@@ -0,0 +1,58 @@
+import { RowDataPacket } from "mysql2/index";
+import {
+	IAuthRequisites,
+	IComment,
+	IProduct,
+	IProductFilterPayload,
+	IProductImage,
+} from "@Shared/types";
+
+export type CommentCreatePayload = Omit<IComment, "id">;
+
+type ProductId = string;
+
+export interface ICommentEntity extends RowDataPacket {
+	comment_id: string;
+	name: string;
+	email: string;
+	body: string;
+	product_id: ProductId;
+}
+
+export interface IProductEntity extends IProduct, RowDataPacket {
+	product_id: ProductId;
+}
+
+export interface IProductSearchFilter extends IProductFilterPayload {}
+
+export type ImageCreatePayload = Omit<IProductImage, "id" | "productId">;
+
+export type ProductCreatePayload = Omit<
+	IProduct,
+	"id" | "comments" | "thumbnail" | "images"
+> & { images: ImageCreatePayload[] };
+
+export interface IProductImageEntity extends RowDataPacket {
+	image_id: string;
+	url: string;
+	product_id: ProductId;
+	main: number;
+}
+
+export interface ProductAddImagesPayload {
+	productId: ProductId;
+	images: ImageCreatePayload[];
+}
+
+export type ImagesRemovePayload = string[];
+
+export interface IUserRequisitesEntity extends IAuthRequisites, RowDataPacket {
+	id: number;
+}
+
+export type SimilarProductPayload = {
+	productId: ProductId;
+	similarProductId: string;
+}[];
+
+export type SimilarProductDeletePayload = ProductId[];
